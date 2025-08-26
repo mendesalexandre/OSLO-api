004abf6834edb1dbd4c76db43c9a7bc79c1af750
Alexandre Teixeira Mendes <alex.fnvip@gmail.com>
2025-08-26 14:16:38 -0400

    WIP

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -46,6 +46,12 @@ return [
             ],
         ],
 
+        // removido da versão 12
+        'redis' => [
+            'driver' => 'redis',
+            'connection' => 'default',
+        ],
+
         'pusher' => [
             'driver' => 'pusher',
             'key' => env('PUSHER_APP_KEY'),
@@ -53,7 +59,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
+                'host' => env('PUSHER_HOST') ?: 'api-' . env('PUSHER_APP_CLUSTER', 'mt1') . '.pusher.com',
                 'port' => env('PUSHER_PORT', 443),
                 'scheme' => env('PUSHER_SCHEME', 'https'),
                 'encrypted' => true,
