eb34495f5aeb93174382c5b050982df6aa47eca0
Alexandre Teixeira Mendes <alex.fnvip@gmail.com>
2025-09-22 21:03:28 -0400

    WIP

--- database/migrations/2025_09_21_094045_criar_natureza_tabela.php
+++ database/migrations/2025_09_21_094045_criar_natureza_tabela.php
@@ -17,8 +17,23 @@ return new class extends Migration
             $table->boolean('is_ativo')->default(true);
             $table->string('nome');
             $table->text('descricao')->nullable();
-            $table->decimal('valor', 10, 2)->nullable();
-            $table->json('opcoes')->nullable();
+
+            $table->integer('prazo_validade_legal')->default(0);
+            $table->boolean('prenotacao_permitir_dias_corridos')->default(true);
+            $table->boolean('prenotacao_permitir_dias_uteis')->default(false);
+            $table->boolean('prenotacao_permitir_prorrogacao')->default(false);
+            $table->boolean('prenotacao_permitir_suspender_cadastro')->default(false);
+            $table->longText('anotacao_personalizada_suspensao')->nullable();
+
+            $table->boolean('qualificacao_permitir_dias_corridos')->default(true);
+            $table->boolean('qualificacao_permitir_dias_uteis')->default(false);
+            $table->boolean('qualificacao_permitir_prorrogacao')->default(false);
+            $table->boolean('qualificacao_permitir_suspender_cadastro')->default(false);
+
+            $table->boolean('reingresso_permitir_dias_corridos')->default(true);
+            $table->boolean('reingresso_permitir_dias_uteis')->default(false);
+            $table->boolean('reingresso_permitir_prorrogacao')->default(false);
+            $table->boolean('reingresso_permitir_suspender_cadastro')->default(false);
 
             $table->unsignedBigInteger('usuario_criacao_id')->nullable()->after('data_exclusao');
             $table->unsignedBigInteger('usuario_alteracao_id')->nullable()->after('usuario_criacao_id');
@@ -35,7 +50,7 @@ return new class extends Migration
             $table->foreign('usuario_exclusao_id')->references('id')->on('usuario')->onDelete('set null');
 
             $table->timestamp('data_cadastro')->useCurrent();
-            $table->timestamp('data_alteracao')->useCurrent();
+            $table->timestamp('data_alteracao')->nullable();
             $table->timestamp('data_exclusao')->nullable();
 
             // Índices para melhor performance
