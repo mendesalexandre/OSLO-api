ac26aa2a07d33deabe501c54d73f0daf6e72b1ce
Alexandre Teixeira Mendes <alex.fnvip@gmail.com>
2025-10-16 21:44:43 -0400

    WIP

--- app/Http/Controllers/ProtocoloController.php
+++ app/Http/Controllers/ProtocoloController.php
@@ -6,9 +6,39 @@ use Illuminate\Http\Request;
 
 class ProtocoloController extends Controller
 {
-    public function criar(Request $request)
+    public function create(Request $request)
     {
-        // Lógica para criar um novo protocolo
+        $validated = $request->validate([
+            'numero' => 'required|integer',
+            'data_prevista' => 'nullable|date',
+            'data_reingresso' => 'nullable|date',
+            'data_limite' => 'nullable|date',
+            'solicitante_id' => 'required|integer',
+            'atendente_id' => 'nullable|integer',
+            'cliente_id' => 'required|integer',
+            'natureza_id' => 'required|integer',
+            'reprotocolo_origem_id' => 'nullable|integer',
+            'tipo_suspensao_id' => 'nullable|integer',
+            'tipo_documento_id' => 'nullable|integer',
+            'vinculo_id' => 'nullable|integer',
+            'entregue' => 'boolean',
+            'etapa_id' => 'nullable|integer',
+            'etapa_usuario_id' => 'nullable|integer',
+            'etapa_anterior_id' => 'nullable|integer',
+            'finalizador_id' => 'nullable|integer',
+            'motivo_cancelamento' => 'nullable|string',
+            'interessado_id' => 'nullable|integer',
+            'tomador_id' => 'nullable|integer',
+            'dias' => 'integer',
+            'is_digital' => 'boolean',
+            'is_parado' => 'boolean',
+            'pago' => 'boolean',
+            'mesa' => 'boolean',
+            'status' => 'string',
+        ]);
+
+        Protocolo::create($validated);
+
         return response()->json(['message' => 'Protocolo criado com sucesso!'], 201);
     }
 }
--- app/Models/Protocolo.php
+++ app/Models/Protocolo.php
@@ -6,5 +6,40 @@ use Illuminate\Database\Eloquent\Model;
 
 class Protocolo extends Model
 {
-    //
+    protected $table = 'protocolo';
+
+    protected $fillable = [
+        'numero',
+        'data_prevista',
+        'data_reingresso',
+        'data_limite',
+        'solicitante_id',
+        'atendente_id',
+        'cliente_id',
+        'natureza_id',
+        'reprotocolo_origem_id',
+        'tipo_suspensao_id',
+        'tipo_documento_id',
+        'vinculo_id',
+        'entregue',
+        'etapa_id',
+        'etapa_usuario_id',
+        'etapa_anterior_id',
+        'finalizador_id',
+        'motivo_cancelamento',
+        'interessado_id',
+        'tomador_id',
+        'dias',
+        'is_digital',
+        'is_parado',
+        'pago',
+        'mesa',
+        'status',
+        'ultima_interacao',
+        'debug',
+        'tags',
+        'data_cadastro',
+        'data_alteracao',
+        'data_exclusao'
+    ];
 }
